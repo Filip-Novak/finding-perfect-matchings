b0e026ce083569b8bf6556651e2e84b0dbffafe9
Filip Novak <filip.novak11@gmail.com>
2020-03-26 18:53:40 +0100

    oprava mainu

--- rekurzia/main.cpp
+++ rekurzia/main.cpp
@@ -10,47 +10,48 @@
 
 #include <basic_impl.hpp>
 
-using namespace std;
 using namespace ba_graph;
 
-void remove_neighbours(vector<vector<int>>& adj_list, set<pair<int, int>>& edges_to_remove, int u, int v) {
-	
-    vector<int> u_neighbours = adj_list[u];
+void remove_neighbours(std::vector<std::vector<Number>> &adj_list, std::set<Location> &edges_to_remove, Number u, Number v) {
 
-    for (int i = 0; i < u_neighbours.size(); i++) {                      //hladame a ukladame si hrany do edges_to_remove, ktore su susedmi zhladiska prveho vrcholu hrany "e",
-                                                                         //nasledne vymazavame v adj_list vrcholy tych hran 
-        if (u_neighbours[i] != v) {
-            if (u <= u_neighbours[i]) {
-                edges_to_remove.insert(make_pair(u, u_neighbours[i]));
-            } else {
-                edges_to_remove.insert(make_pair(u_neighbours[i], u));
-            }
-            adj_list[u_neighbours[i]].erase(remove(adj_list[u_neighbours[i]].begin(), adj_list[u_neighbours[i]].end(), u), adj_list[u_neighbours[i]].end());
-        }
+    std::vector<Number> u_neighbours = adj_list[u.to_int()];
+	int n_i;
+	
+	for (auto neighbour : u_neighbours) {								//hladame a ukladame si hrany do edges_to_remove, ktore su susedmi zhladiska prveho vrcholu hrany "e",
+																		//nasledne vymazavame v adj_list vrcholy tych hran 
+		if (neighbour != v) {
+			if (u <= neighbour) {
+				edges_to_remove.insert(Location(u, neighbour));
+			} else {
+				edges_to_remove.insert(Location(neighbour, u));
+			}
+			n_i = neighbour.to_int();
+			adj_list[n_i].erase(std::remove(adj_list[n_i].begin(), adj_list[n_i].end(), u), adj_list[n_i].end());
+		}
     }
-    adj_list[u].clear();
+    adj_list[u.to_int()].clear();
 }
 
-void recursion(int n, vector<vector<int>> adj_list, set<pair<int, int>> E, vector<pair<int, int>> M) {
-    
-    if (M.size()*2 != n) { 
+void recursion(int n, std::vector<std::vector<Number>> adj_list, std::set<Location> E, std::vector<Location> M, std::function<void(std::vector<Location> &)> callback) {
+
+    if (M.size()*2 != n) {
         if (E.size() >= 1) {
-            set<pair<int, int>> edges_to_remove;                    	//pomocny set v ktorom si budeme pamatat hrany, ktore budeme vymazavat pred prvou rekurziu,
-																		//a nasledne tento set pouzijeme aby sme dostali nase mnoziny do zaciatocneho stavu
-			pair<int, int> e = *next(E.begin(), 0);
-            int v1 = e.first;
-            int v2 = e.second;
-			
+            std::set<Location> edges_to_remove;              			//pomocny set v ktorom si budeme pamatat hrany, ktore budeme vymazavat pred prvou rekurziu,
+                                                                        //a nasledne tento set pouzijeme aby sme dostali nase mnoziny do zaciatocneho stavu
+            Location e = *std::next(E.begin(), 0);
+            Number v1 = e.n1();
+            Number v2 = e.n2();
+
             M.push_back(e);
-			edges_to_remove.insert(e);									//chceme odstranit z E aj hranu co je v perfektnom pareni, tak ju pridame do edges_to_remove
+            edges_to_remove.insert(e);                                  //chceme odstranit z E aj hranu co je v perfektnom pareni, tak ju pridame do edges_to_remove
             remove_neighbours(adj_list, edges_to_remove, v1, v2);
             remove_neighbours(adj_list, edges_to_remove, v2, v1);
 
-            for (auto edge : edges_to_remove) { 						//odstranujeme hrany z E
-				auto pos = E.find(edge);
-				E.erase(pos);
-            }  
-            recursion(n, adj_list, E, M);
+            for (auto edge : edges_to_remove) {                         //odstranujeme hrany z E
+                auto pos = E.find(edge);
+                E.erase(pos);
+            }
+            recursion(n, adj_list, E, M, callback);
 
             //------------------------------------------------------------------------------------  
 
@@ -58,50 +59,61 @@ void recursion(int n, vector<vector<int>> adj_list, set<pair<int, int>> E, vecto
             auto pos = edges_to_remove.find(e);
             edges_to_remove.erase(pos);
 
-            for (auto edge : edges_to_remove) {							//vratenie do zaciatocneho stavu ale bez hrany "e" lebo sme ju vyhodili v prikaze vyssie
-				E.insert(edge);
-                adj_list[edge.first].push_back(edge.second);
-                adj_list[edge.second].push_back(edge.first);
+            for (auto edge : edges_to_remove) {                         //vratenie do zaciatocneho stavu ale bez hrany "e" lebo sme ju vyhodili v prikaze vyssie
+                E.insert(edge);
+                adj_list[edge.n1().to_int()].push_back(edge.n2());
+                adj_list[edge.n2().to_int()].push_back(edge.n1());
             }                                                           //ked skonci for cyklus, tak je to pripravene na rekurziu
-            recursion(n, adj_list, E, M);
-       }
+            recursion(n, adj_list, E, M, callback);
+        }
     } else {
+		/*for (int i = 0; i < M.size(); i++) {
+            std::cout << M[i].n1().to_int() << "-" << M[i].n2().to_int() << " ";
+        }
+        std::cout << std::endl;*/
 		callback(M);
-    } 
+    }
 }
 
-void perfect_matchings_recursion(Graph &G){
+void perfect_matchings_recursion(Graph &G) {
+
+    int n = G.order();                                                  //pocet vrcholov	
+    std::vector<Location> M;                                 			//M je mnozina hran(perfektne parenie)
+    std::set<Location> E;                                    			//mnozina hran v grafe
+    std::vector<std::vector<Number>> adj_list(n);						//pre kazdy vrchol mame zaznamenane vrcholy, ktore s nim susedia
 	
-	int n = G.order();  												//pocet vrcholov	
-	vector<pair<int, int>> M;            								//M je mnozina hran(perfektne parenie)
-    set<pair<int, int>> E;			    			    				//mnozina hran v grafe
-    vector<vector<int>> adj_list(n);        							//pre kazdy vrchol mame zaznamenane vrcholy, ktore s nim susedia
-	
-	for (auto &vertex : G) {											//inicializacia E a adj_list
-		Number v = vertex.n();
-		for (auto u : G[v].neighbours()) {
-			if (v <= u) {
-				E.insert(make_pair(v.to_int(), u.to_int()));
-			}else{
-				E.insert(make_pair(u.to_int(), v.to_int()));
-			}
-			adj_list[v.to_int()].push_back(u.to_int());			
-		}
-	}	
-	recursion(n, adj_list, E, M);
+	Number vertex, v, u;
+	Location e;
+    for (auto &r : G) {													//inicializacia E a adj_list
+        vertex = r.n();
+        for (auto &i : G[vertex]) {
+			v = i.n1();
+			u = i.n2();
+			e = i.l();
+			
+            if (v <= u) {
+                E.insert(e);
+            } else {
+                E.insert(e.reverse());
+            }
+			adj_list[v.to_int()].push_back(u);
+        }
+    }
+	std::function<void(std::vector<Location> &)> callback;
+    recursion(n, adj_list, E, M, callback);
 }
 
-int main(int argc, char** argv) { 
+int main(int argc, char** argv) {
 
-	Graph g(createG());			//priklad grafu
-	addV(g, 0);
-	addV(g, 1);
-	addV(g, 2);
-	addV(g, 3);
-	addE(g, Location(0, 1));
-	addE(g, Location(0, 3));
-	addE(g, Location(1, 2));
-	addE(g, Location(2, 3));
-
+    Graph g(createG());                                                 //priklad grafu
+    addV(g, 0);
+    addV(g, 1);
+    addV(g, 2);
+    addV(g, 3);
+    addE(g, Location(0, 1));
+    addE(g, Location(0, 3));
+    addE(g, Location(1, 2));
+    addE(g, Location(2, 3));
+	
     perfect_matchings_recursion(g);
 }
